6599bb3ffac994b67146af5a89d08d3a25fa37d9
Laura Palacios <lau12000@gmail.com>
2026-05-23 12:50:17 -0500

    Add new code for asge calculate

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,38 @@
 ﻿namespace HelloWorld
 {
-    
-    class program{
+
+    class Program
+    {
         static void Main()
         {
-            const string Name="Jhon";
-            Console.WriteLine($"Hello, World! {Name}");
+            DateOnly dateConverted = new DateOnly();
+            String nameInput;
+            String birthdayInput;
+            Console.WriteLine("Hola, Bienvenido a el calculador de años");
+            Console.WriteLine("Esribe tu nombre:  ");
+            nameInput = Console.ReadLine();
+            Console.WriteLine($"Un gusto conocerte {nameInput} ");
+            Console.WriteLine("Escribe tu fecha de nacimiento (dd/mm/yyyy):  ");
+            birthdayInput = Console.ReadLine();
+            bool isDateValid = DateOnly.TryParse(birthdayInput, out dateConverted);
+            if (isDateValid == false) Console.WriteLine("La fecha no es valida, por favor intentalo de nuevo");
+            var persone = new Person()
+            {
+                Name = nameInput,
+                Bithday = dateConverted,
+                Age = DateTime.Now.Year - dateConverted.Year
+            };
+            Console.WriteLine($"Tu nombre es {persone.Name} y tu fecha de nacimiento es {persone.Bithday}");
+            Console.WriteLine($"Tu edad es: {persone.Age}  años");
+
             Console.WriteLine();
         }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateOnly Bithday { get; set; }
     }
 }
