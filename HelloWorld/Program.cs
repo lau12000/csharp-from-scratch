29a8e47aa4cc80960e01fcbf230761506531b12b
Laura Palacios <lau12000@gmail.com>
2026-05-23 11:55:55 -0500

    Change hello world

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine();
-
+﻿namespace HelloWorld
+{
+    
+    class program{
+        static void Main()
+        {
+            const string Name="Jhon";
+            Console.WriteLine($"Hello, World! {Name}");
+            Console.WriteLine();
+        }
+    }
+}
