45f7f8ce7522ec557487a8cbc650b1d88afe33bf
Laura Palacios <lau12000@gmail.com>
2026-05-23 20:22:49 -0500

    Fix new_copy folder

---
+++ new_copy/conditionals_and_operator/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Conditionals*/
+//Console.WriteLine("Esribe un numero:  ");
+//int num = int.Parse(Console.ReadLine());
+//if (num > 0) Console.WriteLine(" positivo");
+//else if (num < 0) Console.WriteLine("negativo");
+//else Console.WriteLine("Es cero");
+
+/*End conitionals*/
+
+/*operators*/
+Console.WriteLine("Ingrese el primer numero:  ");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero:  ");
+double num2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Suma: {num1 + num2}, Resta: {num1 - num2}, Multiplicación: {num1 * num2}, División: {num1 / num2}" );
+
+/*End operators*/
